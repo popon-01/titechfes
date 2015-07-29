8621c117169fb77cf82714c0de565f5ead8f2b3e
bouro412 <keita.bouro@gmail.com>
2015-07-29 23:41:31 +0900

    change *lib-path* implements

--- global.lisp
+++ global.lisp
@@ -4,4 +4,11 @@
 (defvar *wallimage* nil)
 (defvar *map* (make-array '(10 10)))
 (defvar *keystate* nil)
-(defvar *lib-path* #p"~/Documents/clisp/titechfes/lib/")
+(let ((current-path (load-time-value
+		     (or #.*compile-file-pathname* 
+			 *load-pathname*))))
+  (defvar *dir-path* (directory-namestring current-path)))
+(defvar *lib-path* (merge-pathnames "lib/" *dir-path*))
+
+
+
