6aa07741f753292d441aa6f8706d3132192a2524
matsumura <poponnno1@gmail.com>
2015-08-11 23:24:37 +0900

    radically improve mapchip-player collide

--- player.lisp
+++ player.lisp
@@ -4,80 +4,60 @@
 (define-class player (gameobject)
   (image-r (get-image :player-r))
   (image-l (get-image :player-l))
-  (nx 0)
-  (ny 0)
   (vx 0)
   (vvx 0)
   (vy 0)
-  (in-air nil)
+  (in-air t)
   (jump-cool 0)
   (dash-ok t)
   (while-dash nil)
   (dash-cool 0)
   (dir-right t))
 
+(defun rect-collision-judge (rec1 rec2)
+  (and (< (sdl:x rec1) (sdl:x2 rec2))
+       (< (sdl:x rec2) (sdl:x2 rec1))
+       (< (sdl:y rec1) (sdl:y2 rec2))
+       (< (sdl:y rec2) (sdl:y2 rec1))))
 
-(defmethod collide ((p player) (chip wall))
-  (with-slots (nx ny width height vy
+
+(defmethod collide ((ply player) (chip wall))
+  (with-slots (vx vy
 		  in-air jump-cool
-		  dash-ok while-dash) p 
-    (whens
-     ;;up
-     ((and (< (- (get-x chip) 16) nx)
-	   (< nx (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (- ny (truncate height 2)))
-	   (< (- ny (truncate height 2)) (+ (get-y chip) 16)))
-      (setf ny (+ (get-y chip) 32))
-      (setf vy 0))
-     ;;bottom1
-     ((and (< (- (get-x chip) 16) (- nx 12)) 
-	   (< (- nx 12) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (+ ny (truncate height 2)))
-	   (< (+ ny (truncate height 2)) (+ (get-y chip) 16)))
-      (setf ny (- (get-y chip) 32))
-      (whens ((not in-air)
-	      (setf in-air t) (setf jump-cool 10))
-	     ((and (not dash-ok) (not while-dash))
-	      (setf dash-ok t))))
-     ;;bottom2
-     ((and (< (- (get-x chip) 16) (+ nx 12))
-	   (< (+ nx 12) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (+ ny (truncate height 2)))
-	   (< (+ ny (truncate height 2)) (+ (get-y chip) 16)))
-      (setf ny (- (get-y chip) 32))
-      (whens ((not in-air)
-	      (setf in-air t
-		    jump-cool 10))
-	     ((and (not dash-ok) (not while-dash))
-	      (setf dash-ok t))))
-     ;;left1
-     ((and (< (- (get-x chip) 16) (+ nx (truncate width 2)))
-	   (< (+ nx (truncate width 2)) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (- ny 10))
-	   (< (- ny 10) (+ (get-y chip) 16)))
-      (setf nx (- (get-x chip) 28)))
-     ;;left2
-     ((and (< (- (get-x chip) 16) (+ nx (truncate width 2)))
-	   (< (+ nx (truncate width 2)) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (+ ny 10))
-	   (< (+ ny 10) (+ (get-y chip) 16)))
-      (setf nx (- (get-x chip) 28)))
-     ;;right1
-     ((and (< (- (get-x chip) 16) (- nx (truncate width 2)))
-	   (< (- nx (truncate width 2)) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (- ny 10))
-	   (< (- ny 10) (+ (get-y chip) 16)))
-      (setf nx (+ (get-x chip) 28)))
-     ;;right2
-     ((and (< (- (get-x chip) 16) (- nx (truncate width 2)))
-	   (< (- nx (truncate width 2)) (+ (get-x chip) 16))
-	   (< (- (get-y chip) 16) (+ ny 10))
-	   (< (+ ny 10) (+ (get-y chip) 16)))
-      (setf nx (+ (get-x chip) 28))))))
+		  dash-ok while-dash) ply
+    (sdl:with-rectangle (chip-rec (sdl:rectangle 
+				   :x (- (get-x chip) 16)
+				   :y (- (get-y chip) 16)
+				   :w (width chip)
+				   :h (height chip)))
+      ;;horizontal
+      (sdl:with-rectangle (ply-x-rec (sdl:rectangle 
+				      :x (- (+ (get-x ply) vx) 12)
+				      :y (- (get-y ply) 16)
+				      :w (width ply)
+				      :h (height ply)))
+	(when (rect-collision-judge chip-rec ply-x-rec)
+	  (if (plusp vx)
+	      (setf vx (- (get-x chip) 28 (get-x ply)))
+	      (setf vx (- (get-x chip) -28 (get-x ply))))))
+      ;;vertical
+      (sdl:with-rectangle (ply-y-rec (sdl:rectangle 
+				      :x (- (get-x ply) 12)
+				      :y (- (+ (get-y ply) vy) 16)
+				      :w (width ply)
+				      :h (height ply)))
+	(when (rect-collision-judge chip-rec ply-y-rec)
+	  (if (plusp vy)
+	      (progn (setf vy (- (get-y chip) 32 (get-y ply)))
+		     (whens (in-air
+			     (setf in-air nil
+				   jump-cool 10))
+			    ((and (not dash-ok) (not while-dash))
+			     (setf dash-ok t))))
+	      (setf vy (- (get-y chip) -32 (get-y ply)))))))))
 
 (defmethod update-object ((p player) game)
-  (with-slots (x y nx ny 
-		 width height 
+  (with-slots (x y width height 
 		 vx vvx vy
 		 image image-r image-l
 		 in-air jump-cool
@@ -85,41 +65,37 @@
 		 dir-right) p
     (with-slots (right left jump down shot dash) (keystate game)
       (setf vx 0
-	    image (if dir-right image-r image-l)
-	    nx x
-	    ny y)
+	    image (if dir-right image-r image-l))
+      ;;keyevents
       (whens
        ((and left (not while-dash)) 
 	(decf vx 5) (setf dir-right nil))
        ((and right (not while-dash))
 	(incf vx 5) (setf dir-right t))
-       ((and jump in-air (zerop jump-cool))
-	(setf in-air nil) (setf vy -16))
+       ((and jump (not in-air) (zerop jump-cool))
+	(setf in-air t
+	      vy -16))
        ((and dash dash-ok (zerop dash-cool))
-	(setf while-dash t)
-	(setf dash-ok nil)
-	(if dir-right
-	    (setf vvx 20)
-	    (setf vvx -20))))
-      (when (and while-dash (<= -5 vvx) (<= vvx 5))
-	(setf while-dash nil dash-cool 10))
-      (incf vy)
+	(setf while-dash t
+	      dash-ok nil
+	      vvx (if dir-right 20 -20))))
+      ;;acceleration
+      (if while-dash (setf vy 0) (incf vy))
+      (when (> vy 10) (setf vy 10))
       (whens ((< vvx 0) (incf vvx 2))
-	     ((> vvx 0) (decf vvx 2))
-	     ((> vy 10) (setf vy 10)))
-      (whens
-       ((> jump-cool 0) (decf jump-cool))
-       ((> dash-cool 0) (decf dash-cool)))
-      ;;move vertical
-      (cond (while-dash (incf ny 0)) 
-	    ((< vy -10) (incf ny -10))
-	    (t (incf ny vy)))
+	     ((> vvx 0) (decf vvx 2)))
       (cond ((> vvx 10) (incf vx 10))
 	    ((< vvx -10) (incf vx -10))
 	    (t (incf vx vvx)))
-      (incf nx vx)
+      ;;flags
+      (whens
+       ((> jump-cool 0) (decf jump-cool))
+       ((> dash-cool 0) (decf dash-cool)))
+      (when (and while-dash (<= -5 vvx) (<= vvx 5))
+	(setf while-dash nil dash-cool 10))
+      ;;move
       (dolist (chip (mapchips game))
 	(collide p chip))
-      (setf x nx
-	    y ny))))
+      (incf x vx)
+      (incf y vy))))
   
